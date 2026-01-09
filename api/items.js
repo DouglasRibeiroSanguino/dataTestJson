c600230a58718c1a42c50dc10bdfcadedd0b0be8
Douglas Ribeiro Sanguino <86693183+DouglasRibeiroSanguino@users.noreply.github.com>
2026-01-09 13:10:06 +0000

    Added new file "package.json" and update the items.js

--- api/items.js
+++ api/items.js
@@ -3,26 +3,11 @@ import path from 'path';
 
 export default function handler(req, res) {
   const filePath = path.join(process.cwd(), 'public', 'dataItems.json');
-
   const raw = fs.readFileSync(filePath, 'utf-8');
   const data = JSON.parse(raw);
 
-  const { search, active } = req.query;
-  let result = data.items;
-
-  if (search) {
-    result = result.filter(i =>
-      i.name.toLowerCase().includes(search.toLowerCase())
-    );
-  }
-
-  if (active !== undefined) {
-    result = result.filter(i => i.active === (active === 'true'));
-  }
-
   res.status(200).json({
     status: 'ok',
-    count: result.length,
-    items: result
+    items: data.items
   });
 }
